f11c5691dffb89c2284d826c82e9fc3f523af9e3
Kevin Pavelich <kevin.pavelich@yahoo.com>
2020-02-01 17:16:37 -0800

    working transcription

--- DiscordBot2/index.js
+++ DiscordBot2/index.js
@@ -1,7 +1,47 @@
+require('dotenv').config()
 const Discord = require('discord.js');
 const auth = require('./auth.json');
 const client = new Discord.Client();
+
+const { Readable } = require('stream');
+
+const SILENCE_FRAME = Buffer.from([0xF8, 0xFF, 0xFE]);
+
+class Silence extends Readable {
+  _read() {
+    this.push(SILENCE_FRAME);
+  }
+}
+
+const { Transform } = require('stream')
+
+function convertBufferTo1Channel(buffer) {
+  const convertedBuffer = Buffer.alloc(buffer.length / 2)
+
+  for (let i = 0; i < convertedBuffer.length / 2; i++) {
+    const uint16 = buffer.readUInt16LE(i * 4)
+    convertedBuffer.writeUInt16LE(uint16, i * 2)
+  }
+
+  return convertedBuffer
+}
+
+class ConvertTo1ChannelStream extends Transform {
+  constructor(source, options) {
+    super(options)
+  }
+
+  _transform(data, encoding, next) {
+    next(null, convertBufferTo1Channel(data))
+  }
+}
+
+const googleSpeech = require('@google-cloud/speech')
+
+const googleSpeechClient = new googleSpeech.SpeechClient()
+
 var followingUser = '';
+var defaultChannel;
 
 client.once('ready', () => {
 	console.log('Ready!');
@@ -16,8 +56,8 @@ client.once('ready', () => {
 			}
 		}
 
-		let channel = client.channels.get(guild.systemChannelID || channelID);
-		channel.send("PING PONG I AM ONLINE!");});
+		defaultChannel = client.channels.get(guild.systemChannelID || channelID);
+		defaultChannel.send("PING PONG I AM ONLINE!");});
 });
 
 client.on('message', msg => {
@@ -27,28 +67,68 @@ client.on('message', msg => {
 	}
 })
 
-client.on('voiceStateUpdate', (oldMember, newMember) => {
+client.on('voiceStateUpdate', async (oldMember, newMember) => {
   let newUserChannel = newMember.voiceChannel
   let oldUserChannel = oldMember.voiceChannel
   console.log("USER VOICE CHANNEL STATE CHANGE!");
   console.log("user " + oldMember.user.id);
   console.log("previous channel: " + oldMember.voiceChannelID);
   console.log("new channel: " + newMember.voiceChannelID);
-if (newMember.user.id === followingUser){
-  if((oldUserChannel === undefined && newUserChannel !== undefined) 
-  	|| (oldUserChannel !== undefined && newUserChannel !== undefined && oldUserChannel !== newUserChannel)){
-     // User Joins a voice channel
- 	console.log("Joined " + newUserChannel.id);
- 	newUserChannel.join();
- 	
+	if (newMember.user.id === followingUser){
+	  if((oldUserChannel === undefined && newUserChannel !== undefined) 
+		|| (oldUserChannel !== undefined && newUserChannel !== undefined && oldUserChannel !== newUserChannel)){
+		 // User Joins a voice channel
+		console.log("Joined " + newUserChannel.id);
+		  const connection = await newMember.voiceChannel.join();
+		  const receiver = connection.createReceiver();
+		  defaultChannel.send("I am listening to you");
+		 	connection.playStream(new Silence(), { type: 'opus' });
+		  connection.on('speaking', (user, speaking) => {
+			if (!speaking) {
+			  return
+			}
 
-  } else if(newUserChannel === undefined){
-  	console.log("User left the channel");
-  	oldUserChannel.leave();
-    // User leaves a voice channel
+			console.log(`I'm listening to ${user.username}`)
 
-  }
-}})
+			// this creates a 16-bit signed PCM, stereo 48KHz stream
+			const audioStream = receiver.createOpusStream(user)
+			const requestConfig = {
+			  encoding: 'LINEAR16',
+			  sampleRateHertz: 48000,
+			  languageCode: 'en-US'
+			}
+			const request = {
+			  config: requestConfig,
+			  user:user.username
+			}
+			const recognizeStream = googleSpeechClient
+			  .streamingRecognize(request)
+			  .on('error', console.error)
+			  .on('data', response => {
+				const transcription = response.results
+				  .map(result => result.alternatives[0].transcript)
+				  .join('\n')
+				  .toLowerCase()
+				console.log(`Transcription: ${transcription}`)
+			  })
+
+			const convertTo1ChannelStream = new ConvertTo1ChannelStream()
+
+			audioStream.pipe(convertTo1ChannelStream).pipe(recognizeStream)
+
+			audioStream.on('end', async () => {
+			  console.log('audioStream end')
+					})
+		});
+
+	  }else if(newUserChannel === undefined){
+		console.log("User left the channel");
+		oldUserChannel.leave();
+		// User leaves a voice channel
+
+	  }
+	}
+})
 
 
 client.login(auth.token);
